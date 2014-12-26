695fc5be133ff5be9ca8d951f3d1856148860592
seniztas <seniz.tascioglu@outlook.com>
2014-12-26 16:26:19 +0200

    Dino son

---
+++ Dinoson/Assets/Scripts/MarketButton.js
@@ -0,0 +1,10 @@
+﻿var myGuiBlank : GUISkin;
+
+var btnTexture : Texture;
+
+function OnGUI(){
+	GUI.skin = myGuiBlank;
+	
+	if (GUI.Button(Rect(Screen.width/2+50,Screen.height/2+50,200,50),btnTexture))
+		Debug.Log("Clicked the button with an image");
+}
---
+++ Dinoson/Assets/Scripts/PlayButton.js
@@ -0,0 +1,10 @@
+﻿var myGuiBlank : GUISkin;
+
+var btnTexture : Texture;
+
+function OnGUI(){
+	GUI.skin = myGuiBlank;
+	
+	if (GUI.Button(Rect(Screen.width/2-100,Screen.height/2-100,200,50),btnTexture))
+		Application.LoadLevel("Scene");
+}
